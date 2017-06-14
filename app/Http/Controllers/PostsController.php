e7cf243b9d047409d044993d880b4d5c472fbd04
Jorman Espinoza <jdaniel.espinoza89@gmail.com>
2017-06-14 00:00:40 -0400

    Adding creating post funtionality

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
@@ -34,7 +35,23 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validate the data
+
+        $this->validate($request, array(
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ));
+
+        // Store the data
+
+        $post = new Post;
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+
+        // Redirect to another page
+
+        return redirect()->route('posts.show', $post->id);
     }
 
     /**
